b1450eaddc0751a6f7754e21e8003a5c10ea4bf2
喵喵咪咪 <dalianmao746@gmail.com>
2017-02-07 23:20:33 +0800

    Update get_clipname_from_XML.py

--- get_clipname_from_XML.py
+++ get_clipname_from_XML.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
-#Filename:xmltt
+#Filename:get_clipname_from_XML
 #coding=UTF-8
 
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import os
-import re
-import glob
 import shutil
 
 xmlsample = raw_input('xml: ')
 dom = xml.dom.minidom.parse(xmlsample)
 content = dom.documentElement
-#if content.hasAttribute("version"):
-#  print "Version is: %s" % content.getAttribute("version")
+if content.hasAttribute("version"):
+  print "Version is: %s" % content.getAttribute("version")
 
 clips = content.getElementsByTagName("file")
 ineed = open('/Users/miao/Desktop/ineed.txt','w')
@@ -25,11 +23,11 @@ for clip in clips:
 
 with open('/Users/miao/Desktop/ineed.txt','r') as lines:
   filename_to_copy = set(line.rstrip() for line in lines)
-  dir = '/Volumes/DIT_RAID_2T/800_BASTARDS/PIX'
-  for root,dirs,files in os.walk(dir):
+  dir = '/Volumes/DIT_RAID_2T/'
+  for root,dirs,files in os.walk(os.path.join(dir)):
     for file in files:
       if os.path.splitext(file)[0] in filename_to_copy:
-        srcPath = os.path.join(dir,file)
+        srcPath = os.path.join(root,file)
         desPath = '/Volumes/DIT_RAID_2T/files'
         shutil.copy(srcPath,desPath)
 #    elif os.path.splitext(file)[1] == '.JPG':
